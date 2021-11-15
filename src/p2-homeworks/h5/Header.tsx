4c03e2d8a74b32ef9095efaa95e3340d68031a1e
Kseniya Melnik <kseniyamelnnik@gmail.com>
2021-11-15 14:00:32 +0300

    refactor: change styles for header

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react'
 import {NavLink} from "react-router-dom";
 import s from './Header.module.css'
+import icon from './assets/Cat-Clean-32.png'
 
 function Header() {
     return (
        <div className={s.container}>
-           <div className={s.hoveredElement}> </div>
+           <img src={icon} alt={icon} />
            <div className={s.navbar}>
                <NavLink to={'/pre-junior'} className={({isActive}) =>(isActive ? s.active : s.link)}>PreJunior</NavLink>
                <NavLink to={'/junior'} className={({isActive}) =>(isActive ? s.active : s.link)}>Junior</NavLink>
